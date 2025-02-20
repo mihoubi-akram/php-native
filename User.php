b6b790a4ef968da8822a58cdc6d6a3cf7bcb2383
Akram Fawzi Mihoubi <mihhakram@gmail.com>
2025-02-20 21:47:39 +0100

    add getAllUsers

--- User.php
+++ User.php
@@ -19,6 +19,25 @@ class User {
         $this->password = password_hash($password, PASSWORD_DEFAULT);
     }
 
+
+    public static function getAllUsers() {
+        try {
+            $pdo = new PDO('', '', '');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $stmt = $pdo->query('SELECT name, email, password FROM users');
+            $users = [];
+
+            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $users[] = new User($row['name'], $row['email'], $row['password']);
+            }
+
+            return $users;
+
+        } catch (PDOException $e) {
+            echo($e->getMessage());
+        }
+    }
    
 }
 
